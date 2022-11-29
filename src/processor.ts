fb829cb0c4bb5e099c2ed05ab4cc8ef7c11128b7
dzhelezov <dzhelezov@gmail.com>
2022-11-29 13:02:40 +0100

    initial commit

--- src/processor.ts
+++ src/processor.ts
@@ -1,37 +1,57 @@
 import { TypeormDatabase } from "@subsquid/typeorm-store";
 import {EvmBatchProcessor} from '@subsquid/evm-processor'
+import { events, Contract } from "./abi/weth";
+import assert from "assert";
+import { ethers } from "ethers";
+
+const WETH_CONTRACT='0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
 
 const processor = new EvmBatchProcessor()
   .setDataSource({
-    // uncomment and set ETHEREUM_MAINNET_WSS to 
-    // a websocket JSON-RPC Etheruem mainnet endpoint in
-    // order to enable contract state queries:
-    // chain: process.env.ETHEREUM_MAINNET_WSS,
-    archive: 'https://eth.archive.subsquid.io',
+    chain: 'https://rpc.ankr.com/eth',
+    archive: 'https://eth-stage1.archive.subsquid.io',
   })
-  .addTransaction([
-    '0x0000000000000000000000000000000000000000'
+  .addLog([
+    WETH_CONTRACT
   ], {
-    range: {
-      from: 10_000_000
-    },
+    filter: [[
+      events.Deposit.topic, events.Withdrawal.topic
+    ]],
     data: {
-      transaction: {
-        from: true,
-        input: true,
-        to: true
-      }
-    }
-  });
-
+      evmLog: {
+          topics: true,
+          data: true,
+      },
+    } as const,
+  })
+  
+let deposited =  ethers.BigNumber.from(0)
+let withdrawn = ethers.BigNumber.from(0)
 
 processor.run(new TypeormDatabase(), async (ctx) => {
+  let b = null;
   for (let c of ctx.blocks) {
     for (let i of c.items) {
       // apply arbitrary data transformation logic here
       // use ctx.store to persist the data
-      ctx.log.info(i, "Next item:")
+      b = c.header;
+      if(i.kind !== 'evmLog') {
+        continue
+      }
+      if (i.evmLog.topics[0] == events.Deposit.topic) {
+        const amt = events.Deposit.decode(i.evmLog).wad
+        deposited = deposited.add(amt)
+        continue
+      }
+      if (i.evmLog.topics[0] == events.Withdrawal.topic) {
+        const amt = events.Withdrawal.decode(i.evmLog).wad
+        withdrawn = withdrawn.add(amt)
+        continue
+      }
     }
   }
+  assert(b, 'must be defined')
+  let supply = await (new Contract({ _chain: ctx._chain, block: b}, WETH_CONTRACT).totalSupply())
+  ctx.log.info(`Deposited: ${deposited.toString()}, Withdrawn: ${withdrawn.toString()}, Diff: ${deposited.sub(withdrawn).toString()}, Supply: ${supply.toString()}`)
 });
 
